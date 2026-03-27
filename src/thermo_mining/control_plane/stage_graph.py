f393e5c69a006d9b65bf084360cc59d0960aed47
koishizzp <annicebailey5407@gmail.com>
2026-03-27 18:24:14 +0800

    feat(control-plane): add schemas and stage graph

---
+++ src/thermo_mining/control_plane/stage_graph.py
@@ -0,0 +1,36 @@
+from .schemas import BundleType
+
+STAGE_ORDERS: dict[BundleType, list[str]] = {
+    "paired_fastq": [
+        "fastp",
+        "spades",
+        "prodigal",
+        "prefilter",
+        "mmseqs_cluster",
+        "temstapro_screen",
+        "protrek_recall",
+        "foldseek_confirm",
+        "rerank_report",
+    ],
+    "contigs": [
+        "prodigal",
+        "prefilter",
+        "mmseqs_cluster",
+        "temstapro_screen",
+        "protrek_recall",
+        "foldseek_confirm",
+        "rerank_report",
+    ],
+    "proteins": [
+        "prefilter",
+        "mmseqs_cluster",
+        "temstapro_screen",
+        "protrek_recall",
+        "foldseek_confirm",
+        "rerank_report",
+    ],
+}
+
+
+def build_stage_order(bundle_type: BundleType) -> list[str]:
+    return STAGE_ORDERS[bundle_type]
